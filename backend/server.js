239ae24953cb0184c209f3c7971a91446e715340
Devesh Patil <155056579+devpatony@users.noreply.github.com>
2025-07-14 10:48:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,6 @@ app.use('*', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-// For local development
 if (process.env.NODE_ENV !== 'production') {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
